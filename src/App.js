394c330a2224be706da8d394597f4084781953f2
Vaibhavi <porevaibhavi5@gmail.com>
2021-12-16 19:45:06 +0530

    Added Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
+import {Route, Redirect, Switch} from 'react-router-dom';
+
 import './App.css';
 import Header from "./MyComponents/Header";
 import Home from "./MyComponents/Home";
 import Footer from "./MyComponents/Footer";
-// import Blog from "./MyComponents/Blog";
-// import Resume from "./MyComponents/Resume";
-// import Projects from "./MyComponents/Projects";
+import Blog from "./MyComponents/Blog";
+import Resume from "./MyComponents/Resume";
+import Projects from "./MyComponents/Projects";
 import React from 'react';
 
 function App() {
   return (
     <>
       <Header/>
-      <Home/>
+      <Switch>
+        <Route exact path='/'>
+          <Redirect to='/home'/>
+        </Route>
+        <Route path='/home'>
+          <Home/>
+        </Route>
+        <Route path='/resume'>
+          <Resume/>
+        </Route>
+        <Route path='/projects'>
+          <Projects/>
+        </Route>
+        <Route path='/blog'>
+          <Blog/>
+        </Route>
+      </Switch>
+
       <Footer/>     
     </>
   );
--- src/MyComponents/Header.js
+++ src/MyComponents/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import React from 'react';
 import '../assets/Header.css'
 function Header(){
@@ -6,25 +7,25 @@ function Header(){
         <nav className="navbar navbar-expand-lg navbar-light fixed-top header">
             <div className="container-fluid">
 
-                <a className="navbar-brand ms-5 fs-4 fw-bolder" href="/home">Vaibhavi Pore</a>
+                <NavLink className="navbar-brand ms-5 fs-4 fw-bolder" to="/home">Vaibhavi Pore</NavLink>
 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/home">Home</a>
+                    <NavLink className="nav-link active" aria-current="page" to="/home">Home</NavLink>
                     </li>
                     
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/resume">Resume</a>
+                    <NavLink className="nav-link active" aria-current="page" to="/resume">Resume</NavLink>
                     </li>
                     
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/projects">Projects</a>
+                    <NavLink className="nav-link active" aria-current="page" to="/projects">Projects</NavLink>
                     </li>
                     
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/blog">Blog</a>
+                    <NavLink className="nav-link active" aria-current="page" to="/blog">Blog</NavLink>
                     </li>
                 </ul>
                 </div>
