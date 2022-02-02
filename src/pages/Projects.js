3b35d6bfe233c5a3703692899f46dd360f693a12
Vaibhavi <porevaibhavi5@gmail.com>
2022-02-02 22:49:25 +0530

    Updated Projects Page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,38 +1,44 @@
 import React from 'react';
 import '../assets/Projects.css'
 import { NavLink } from 'react-router-dom';
+import crossword from '../assets/crossword.png'
 
 function Projects(){
     return (
         <>
             <h1 className='display-6 text-center' style={{fontFamily: "Poppins"}}>PROJECTS</h1>
-            <div>
-                <NavLink to={`/projects/crossword`}>CrossWord</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/reefood`}>ReeFood</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/studentwiki`}>StudentWiki</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/travelpro`}>TravelPro</NavLink>
+            <div className='row mx-3'>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <img src={crossword} className='projectPic'/>
+                    <NavLink to={`/projects/crossword`}>CrossWord</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/reefood`}>ReeFood</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/studentwiki`}>StudentWiki</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/travelpro`}>TravelPro</NavLink>
+                </div>
             </div>
             <h3>Figma Designs</h3>
-            <div>
-                <NavLink to={`/projects/ostello`}>Ostello</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/rebooks`}>ReBooks</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/studentwikiUI`}>StudentWiki</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/restrorder`}>RestrOrder</NavLink>
-            </div>
-            <div>
-                <NavLink to={`/projects/liveasy`}>LivEasy</NavLink>
+            <div className='row mx-3'>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/ostello`}>Ostello</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/rebooks`}>ReBooks</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/studentwikiUI`}>StudentWiki</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/restrorder`}>RestrOrder</NavLink>
+                </div>
+                <div className='col shadow p-3 mb-5 me-3 projects rounded text-center'>
+                    <NavLink to={`/projects/liveasy`}>LivEasy</NavLink>
+                </div>
             </div>
         </>
     );
