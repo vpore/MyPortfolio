70877729fd74592755cdd54f772b8d5e1a2b30ad
Vaibhavi <porevaibhavi5@gmail.com>
2022-02-11 22:56:57 +0530

    Modified TravelPro[ekdum thodu sa :'| ]

--- src/pages/TravelPro.js
+++ src/pages/TravelPro.js
@@ -2,7 +2,11 @@
 const TravelPro = () => {
     return(
         <>
-            
+            <article style={{width: '800px'}}>
+                <h3>Introduction</h3>
+                <p>TravelPro is a bill-generating application developed using Java programming language for tourism industry. It computes the bill after accepting the required input data from the user.</p>
+
+            </article>
         </>
     );
 };
